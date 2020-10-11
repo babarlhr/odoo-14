8e67b5645748499923556d92ea4a52647667b5c5
surgi-tech <59487271+surgi-tech@users.noreply.github.com>
2020-10-11 11:57:38 +0200

    surgi_ess\__manifest__.py

--- surgi_ess/__manifest__.py
+++ surgi_ess/__manifest__.py
@@ -20,7 +20,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','hr','hr_attendance','hr_holidays','project','surgi_evaluation','surgi_attendance_sheet','attendance_sheet_extra','hr_appraisal'],
+    'depends': ['base','hr','hr_attendance','hr_holidays','project','surgi_evaluation','surgi_attendance_sheet','attendance_sheet_extra','hr_appraisal','emp_attendance_google_map_app'],
 
     # always loaded
     'data': [
